27ddf14f00f2ab20525baa4f310a3344868ed1bc
liuzhe <liuzhe@yuanjisuan.cn>
2020-02-18 16:04:00 +0800

    删除大数据模块

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -7,11 +7,6 @@ import './index.css';
 import loadable from '../../utils/lazyLoad';
 
 const Home = loadable(() => import('../Home'));
-const Hdfscluster = loadable(() => import('../HdfsCluster'));
-const Bigdata = loadable(() => import('../Bigdata'));
-const Yarncluster = loadable(() => import('../YarnCluster'));
-const Hbasecluster = loadable(() => import('../HbaseCluster'));
-const Zkui = loadable(() => import('../Zkui'));
 const NewApp = loadable(() => import('../NewApp'));
 const Personal = loadable(() => import('../Personal'));
 const Notice = loadable(() => import('../Notice'));
@@ -22,11 +17,6 @@ export default class Container extends React.Component {
         return (
             <Content className="home-content" style={this.props.style}>
                 <Route path="/home" component={Home} />
-                <Route path="/hdfscluster" component={Hdfscluster} />
-                <Route path="/bigdata" component={Bigdata} />
-                <Route path="/yarncluster" component={Yarncluster} />
-                <Route path="/hbasecluster" component={Hbasecluster} />
-                <Route path="/zkui" component={Zkui} />
                 <Route path="/newapp" component={NewApp} />
                 <Route path="/personal" component={Personal} />
                 <Route path="/notice" component={Notice} />
