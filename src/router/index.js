768d1f6b448997dd3121ac5d1274eabbfdf3d80d
liuzhe <liuzhe@yuanjisuan.cn>
2020-02-18 10:37:03 +0800

    路由配置

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Switch, Redirect, HashRouter as Router, Route } from "react-router-dom";
+import loadable from '../utils/lazyLoad';
+
+const Homepage = loadable(() => import('../pages/HomePage'));
+const Login = loadable(() => import('../pages/Login'));
+const Register = loadable(() => import('../pages/Register'));
+const Details = loadable(() => import('../pages/Details'));
+const ErrorPage = loadable(() => import('../pages/ErrorPage'));
+const About = loadable(() => import('../pages/About'));
+
+
+export default class EarthRouter extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path={["/home", "/hdfscluster", "/bigdata", "/yarncluster", "/hbasecluster", "/zkui", "/newapp", "/personal", "/notice"]}>
+                        <Homepage />
+                    </Route>
+                    <Route exact path="/details">
+                        <Details />
+                    </Route>
+                    <Route exact path="/">
+                        <Redirect to="/login" />
+                    </Route>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/about" component={About} />
+                    <Route render={() => (<ErrorPage />)} />
+                </Switch>
+            </Router>
+        );
+    };
+};
