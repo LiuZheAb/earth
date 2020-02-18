da2011a8c04a5768825a2784330f2fcedcdb47bd
liuzhe <liuzhe@yuanjisuan.cn>
2020-02-18 16:01:03 +0800

    删除map-reduce和大数据接口地址

--- src/assets/urls/index.js
+++ src/assets/urls/index.js
@@ -1,4 +1,1 @@
 export let apiurl = "http://139.219.15.47:6001/";
-export let bigdataUrl = "http://192.168.2.142";
-export let mrUrl = "http://192.168.2.112:6002/";
-export let bigdataUrl2 = "http://192.168.2.142";
